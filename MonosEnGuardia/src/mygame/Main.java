cec51635dea48cd9ae0902af04606588b2d12ca3
Suddapon <JonathanCarrera14@hotmail.com>
2024-04-21 18:21:28 -0600

    Musica + textura corrigida
    Se añadio musica de fondo y se corrigio la textura de la flor

--- MonosEnGuardia/src/mygame/Main.java
+++ MonosEnGuardia/src/mygame/Main.java
@@ -1,8 +1,11 @@
 package mygame;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.audio.AudioData.DataType;
+import com.jme3.audio.AudioNode;
 import com.jme3.light.AmbientLight;
 import com.jme3.material.Material;
+import com.jme3.material.RenderState;
 import com.jme3.math.ColorRGBA;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
@@ -23,12 +26,19 @@ public class Main extends SimpleApplication {
     }
 
     private void initScene() {
+        AudioNode music = new AudioNode(assetManager, "Sounds/musica.wav", true);
+        music.setPositional(false);
+        music.setLooping(true);
+        music.setVolume(0.7f);
+        rootNode.attachChild(music);
+        music.play();
+        
         // Creamos el suelo del escenario
         Box ground = new Box(10, 0.1f, 10);
         Geometry groundGeom = new Geometry("Ground", ground);
         
         // Cargamos la textura de pasto
-        Texture grassTexture = assetManager.loadTexture("Textures/Terrain/grass4.jpg");
+        Texture grassTexture = assetManager.loadTexture("Textures/Terrain/grass3.jpg");
         Material groundMat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
         groundMat.setTexture("ColorMap", grassTexture); // Asignamos la textura al material
         groundGeom.setMaterial(groundMat);
@@ -45,6 +55,17 @@ public class Main extends SimpleApplication {
         model.rotate(0,135,0);
         model.setLocalTranslation(0, -.25f, 0); // Posicionamos la torre
         rootNode.attachChild(model);
+        //Flowers
+        Spatial flower1 = assetManager.loadModel("Models/flor3.j3o" );
+        Material flowermat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        Texture flowertxt = assetManager.loadTexture("Textures/Flowers/flor3.png");
+        flowermat.getAdditionalRenderState().setBlendMode(RenderState.BlendMode.Alpha);
+        flowermat.setTexture("ColorMap", flowertxt);
+        flower1.setMaterial(flowermat);
+        flower1.scale(1);
+        model.setLocalTranslation(-6, -.25f, 0);
+        rootNode.attachChild(flower1);
+        
         
         // Agregamos la luz al escenario
         initLight();
