5643ab3cb79ee6d1c25f6a14c1bdb857b1da6efb
Suddapon <JonathanCarrera14@hotmail.com>
2024-05-03 02:06:15 -0600

    expansión de terreno + torre enemiga

--- MonosEnGuardia/src/mygame/Main.java
+++ MonosEnGuardia/src/mygame/Main.java
@@ -4,14 +4,17 @@ import com.jme3.app.SimpleApplication;
 import com.jme3.audio.AudioData.DataType;
 import com.jme3.audio.AudioNode;
 import com.jme3.light.AmbientLight;
+import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
 import com.jme3.material.RenderState;
+import com.jme3.material.RenderState.BlendMode;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Box;
+import com.jme3.scene.shape.Sphere;
 import com.jme3.system.AppSettings;
 import com.jme3.texture.Texture;
 
@@ -45,6 +48,8 @@ public class Main extends SimpleApplication {
         
         // Creamos el suelo del escenario
         Spatial ground = assetManager.loadModel("Models/escenario.j3o" );
+        Spatial ground2 = assetManager.loadModel("Models/escenario.j3o" );
+        
         // Cargamos la textura de pasto
         Texture grassTexture = assetManager.loadTexture("Textures/Terrain/pasto.png");
         Material groundMat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
@@ -53,7 +58,17 @@ public class Main extends SimpleApplication {
         ground.scale(6.5f);
         ground.setLocalTranslation(0, -2.5f, 0);
         rootNode.attachChild(ground);
-
+        
+         // Cargamos la textura de pasto
+        Texture grassTexture2 = assetManager.loadTexture("Textures/Terrain/pastodark.png");
+        Material groundMat2 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        groundMat2.setTexture("ColorMap", grassTexture2); // Asignamos la textura al material
+        ground2.setMaterial(groundMat2);
+        ground2.scale(6.5f);
+        ground2.setLocalTranslation(0, -2.5f, 20.25f);
+        rootNode.attachChild(ground2);
+        
+        
         // Creamos una torre (que luego será la Banana Dorada)
         Spatial model = assetManager.loadModel("Models/monkey.j3o" );
         Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
@@ -64,162 +79,90 @@ public class Main extends SimpleApplication {
         model.rotate(0,3.2f,0);
         model.setLocalTranslation(0, -.3f, -5); // Posicionamos la torre
         rootNode.attachChild(model);
+        
+        //torre enemiga
+        Spatial torredark = assetManager.loadModel("Models/tower.j3o" );
+        Material darkm = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        Texture darkt = assetManager.loadTexture("Textures/torredark.png");
+        darkm.setTexture("ColorMap", darkt);
+        torredark.setMaterial(darkm);
+        torredark.scale(3.5f);
+        torredark.rotate(0,0,0);
+        torredark.setLocalTranslation(0,.5f, 25); // Posicionamos la torre
+        rootNode.attachChild(torredark);
        
+        DirectionalLight sun = new DirectionalLight();
+        sun.setDirection(new Vector3f(-0.1f, -0.7f, -1.0f).normalizeLocal());
+        rootNode.addLight(sun);
+        
         //Flowers
-        Spatial flower1 = assetManager.loadModel("Models/flor3.j3o" );
-        Material flowermat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        Texture flowertxt = assetManager.loadTexture("Textures/Flowers/flor3.png");
-        flowermat.getAdditionalRenderState().setBlendMode(RenderState.BlendMode.Alpha);
-        flowermat.setTexture("ColorMap", flowertxt);
-        flowermat.getAdditionalRenderState().setBlendMode(RenderState.BlendMode.Alpha);
-        flower1.setMaterial(flowermat);
-        flower1.scale(1);
-        flower1.setLocalTranslation(-6, 0, 0);
-        rootNode.attachChild(flower1);
-        
-        //Flowers 
-        
-        Spatial flower2 = assetManager.loadModel("Models/flor3.j3o" );
-        Material flowermat2 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        Texture flowertxt2 = assetManager.loadTexture("Textures/Flowers/flor1.png");
-        flowermat2.getAdditionalRenderState().setBlendMode(RenderState.BlendMode.Alpha);
-        flowermat2.setTexture("ColorMap", flowertxt2);
-        flower2.setMaterial(flowermat2);
-        flower2.scale(1);
-        flower2.setLocalTranslation(-6, 0, 6);
-        rootNode.attachChild(flower2);
-        
-        
-        Spatial flower3 = assetManager.loadModel("Models/flor3.j3o" );
-        Material flowermat3 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        Texture flowertxt3 = assetManager.loadTexture("Textures/Flowers/flor2.png");
-        flowermat3.getAdditionalRenderState().setBlendMode(RenderState.BlendMode.Alpha);
-        flowermat3.setTexture("ColorMap", flowertxt3);
-        flower3.setMaterial(flowermat3);
-        flower3.scale(1);
-        flower3.setLocalTranslation(6, 0, -6);
-        rootNode.attachChild(flower3);
-        
-        Spatial flower4 = assetManager.loadModel("Models/flor3.j3o" );
-        Material flowermat4 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        Texture flowertxt4 = assetManager.loadTexture("Textures/Flowers/flor3.png");
-        flowermat4.getAdditionalRenderState().setBlendMode(RenderState.BlendMode.Alpha);
-        flowermat4.setTexture("ColorMap", flowertxt4);
-        flower4.setMaterial(flowermat3);
-        flower4.scale(1);
-        flower4.setLocalTranslation(6, 0, 0);
-        rootNode.attachChild(flower4);
-        
-        
-        // Quinta flor (flor2)
-        Spatial flower5 = assetManager.loadModel("Models/flor3.j3o");
-        Material flowerMat5 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        Texture flowerTxt5 = assetManager.loadTexture("Textures/Flowers/flor2.png");
-        flowerMat5.getAdditionalRenderState().setBlendMode(RenderState.BlendMode.Alpha);
-        flowerMat5.setTexture("ColorMap", flowerTxt5);
-        flower5.setMaterial(flowerMat5);
-        flower5.scale(1);
-        flower5.setLocalTranslation(-6, 0, -6);
-        rootNode.attachChild(flower5);
+        // Array de posiciones de las flores
+        Vector3f[] flowerPositions = {
+            new Vector3f(-6, 0, 6),
+            new Vector3f(6, 0, -6),
+            new Vector3f(6, 0, 0),
+            new Vector3f(-6, 0, -6),
+            new Vector3f(-4, 0, 4),
+            new Vector3f(9, 0, -3),
+            new Vector3f(6, 0, 3),
+            new Vector3f(9, 0, 3),
+            new Vector3f(-9, 0, 7),
+            new Vector3f(-8, 0, 2),
+            new Vector3f(8, 0, 7)
+        };
 
-        // Sexta flor (flor3)
-        Spatial flower6 = assetManager.loadModel("Models/flor3.j3o");
-        Material flowerMat6 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        Texture flowerTxt6 = assetManager.loadTexture("Textures/Flowers/flor3.png");
-        flowerMat6.getAdditionalRenderState().setBlendMode(RenderState.BlendMode.Alpha);
-        flowerMat6.setTexture("ColorMap", flowerTxt6);
-        flower6.setMaterial(flowerMat6);
-        flower6.scale(1);
-        flower6.setLocalTranslation(-4, 0, 4);
-        rootNode.attachChild(flower6);
+        // Array de texturas de las flores
+        String[] flowerTextures = {
+            "Textures/Flowers/flor1.png",
+            "Textures/Flowers/flor2.png",
+            "Textures/Flowers/flor3.png",
+            "Textures/Flowers/flor2.png",
+            "Textures/Flowers/flor3.png",
+            "Textures/Flowers/flor1.png",
+            "Textures/Flowers/flor1.png",
+            "Textures/Flowers/flor3.png",
+            "Textures/Flowers/flor1.png",
+            "Textures/Flowers/flor2.png",
+            "Textures/Flowers/flor3.png"
+        };
 
-        // Séptima flor (flor1)
-        Spatial flower7 = assetManager.loadModel("Models/flor3.j3o");
-        Material flowerMat7 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        Texture flowerTxt7 = assetManager.loadTexture("Textures/Flowers/flor1.png");
-        flowerMat7.getAdditionalRenderState().setBlendMode(RenderState.BlendMode.Alpha);
-        flowerMat7.setTexture("ColorMap", flowerTxt7);
-        flower7.setMaterial(flowerMat7);
-        flower7.scale(1);
-        flower7.setLocalTranslation(9, 0, -3);
-        rootNode.attachChild(flower7);
+        // Crear y agregar las flores
+        for (int i = 0; i < flowerPositions.length; i++) {
+            Spatial flower = assetManager.loadModel("Models/flor3.j3o");
+            Material flowerMat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+            Texture flowerTxt = assetManager.loadTexture(flowerTextures[i]);
+            flowerMat.setTexture("ColorMap", flowerTxt);
+            flowerMat.getAdditionalRenderState().setBlendMode(BlendMode.Alpha);
+            flower.setMaterial(flowerMat);
+            flower.scale(1);
+            flower.setLocalTranslation(flowerPositions[i]);
+            rootNode.attachChild(flower);
+        }
 
-        // Octava flor (flor2)
-        Spatial flower8 = assetManager.loadModel("Models/flor3.j3o");
-        Material flowerMat8 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        Texture flowerTxt8 = assetManager.loadTexture("Textures/Flowers/flor1.png");
-        flowerMat8.getAdditionalRenderState().setBlendMode(RenderState.BlendMode.Alpha);
-        flowerMat8.setTexture("ColorMap", flowerTxt8);
-        flower8.setMaterial(flowerMat8);
-        flower8.scale(1);
-        flower8.setLocalTranslation(6, 0, 3);
-        rootNode.attachChild(flower8);
 
-        // Novena flor (flor3)
-        Spatial flower9 = assetManager.loadModel("Models/flor3.j3o");
-        Material flowerMat9 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        Texture flowerTxt9 = assetManager.loadTexture("Textures/Flowers/flor3.png");
-        flowerMat9.getAdditionalRenderState().setBlendMode(RenderState.BlendMode.Alpha);
-        flowerMat9.setTexture("ColorMap", flowerTxt9);
-        flower9.setMaterial(flowerMat9);
-        flower9.scale(1);
-        flower9.setLocalTranslation(9, 0, 3);
-        rootNode.attachChild(flower9);
 
-       // Décima flor (flor1)
-        Spatial flower10 = assetManager.loadModel("Models/flor3.j3o");
-        Material flowerMat10 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        Texture flowerTxt10 = assetManager.loadTexture("Textures/Flowers/flor1.png");
-        flowerMat10.getAdditionalRenderState().setBlendMode(RenderState.BlendMode.Alpha);
-        flowerMat10.setTexture("ColorMap", flowerTxt10);
-        flower10.setMaterial(flowerMat10);
-        flower10.scale(1);
-        flower10.setLocalTranslation(-9, 0, 7);
-        rootNode.attachChild(flower10);
-
-        // Undécima flor (flor2)
-        Spatial flower11 = assetManager.loadModel("Models/flor3.j3o");
-        Material flowerMat11 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        Texture flowerTxt11 = assetManager.loadTexture("Textures/Flowers/flor2.png");
-        flowerMat11.getAdditionalRenderState().setBlendMode(RenderState.BlendMode.Alpha);
-        flowerMat11.setTexture("ColorMap", flowerTxt11);
-        flower11.setMaterial(flowerMat11);
-        flower11.scale(1);
-        flower11.setLocalTranslation(-8, 0, 2);
-        rootNode.attachChild(flower11);
-        
-        // Duodécima flor (flor3)
-        Spatial flower12 = assetManager.loadModel("Models/flor3.j3o");
-        Material flowerMat12 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        Texture flowerTxt12 = assetManager.loadTexture("Textures/Flowers/flor3.png");
-        flowerMat12.getAdditionalRenderState().setBlendMode(RenderState.BlendMode.Alpha);
-        flowerMat12.setTexture("ColorMap", flowerTxt12);
-        flower12.setMaterial(flowerMat12);
-        flower12.scale(1);
-        flower12.setLocalTranslation(8, 0, 7);
-        rootNode.attachChild(flower12);
-        
-        // Agregamos la luz al escenario
-        initLight();
-        
         // Caminito
         Spatial caminito = assetManager.loadModel("Models/Caminito.j3o");
         Material caminitomat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
         Texture caminitotxt = assetManager.loadTexture("Textures/Terrain/caminito.png");
         caminitomat.setTexture("ColorMap",caminitotxt);
         caminito.setMaterial(caminitomat);
-        caminito.scale(3.4f);
+        caminito.scale(3.5f);
         caminito.rotate(0,7.9f,0);
         caminito.setLocalTranslation(.2f, -.8f, 3.6f);
         rootNode.attachChild(caminito);
         
-    }
-
-    private void initLight() {
-        // Configuramos la luz ambiental
-        AmbientLight ambientLight = new AmbientLight();
-        ambientLight.setColor(ColorRGBA.White.mult(0.3f)); // Color y intensidad de la luz ambiental
-        rootNode.addLight(ambientLight);
+        // Caminito 2
+        Spatial caminito2 = assetManager.loadModel("Models/Caminito.j3o");
+        Material caminitomat2 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        Texture caminitotxt2 = assetManager.loadTexture("Textures/Terrain/caminito2.png");
+        caminitomat2.setTexture("ColorMap",caminitotxt2);
+        caminito2.setMaterial(caminitomat2);
+        caminito2.scale(3.5f);
+        caminito2.rotate(0,1.55f,0);
+        caminito2.setLocalTranslation(.2f, -.8f, 16.65f);
+        rootNode.attachChild(caminito2);
+        
     }
 
     @Override
